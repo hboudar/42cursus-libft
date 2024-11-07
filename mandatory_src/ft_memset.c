82649d9b721e83991907f6f601ff5f01e48221a6
Cloneg7 <boudarhamza12@gmail.com>
2024-11-07 11:00:11 +0100

    mdf

--- mandatory_src/ft_memset.c
+++ mandatory_src/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 11:39:39 by hboudar           #+#    #+#             */
-/*   Updated: 2024/05/15 15:15:51 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/11/07 10:59:39 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,36 @@ void	*ft_memset(void *s, int c, size_t len)
 	}
 	return (s);
 }
+
+// int main()
+// {
+//     char    *str =  "S3IB 3LK HADSHI";
+//     void    *ptr =  NULL;
+//     long    add  =  (long)str;
+    
+//     for (int i = 0; i < 8; i++)
+//     {
+//         memset((char *)&ptr + i, add % 256, 1);
+//         add /= 256;
+//     }
+//     printf("%s\n", (char *)ptr);
+// }
+
+
+// int main() {
+//     int arr[] = {42, 1337, 2023, 4096, 54, 6548, 58};  // Example integer array
+//     void *ptr = NULL;
+//     long add = (long)arr;  // Store the address of arr in a long
+
+//     for (int i = 0; i < 8; i++) {
+//         memset((char *)&ptr + i, add % 256, 1);
+//         add /= 256;
+//     }
+
+//     // Use ptr as an int pointer and print the values
+//     int *intPtr = (int *)ptr;
+//     for (int i = 0; i < 7; i++) {
+//         printf("%d ", intPtr[i]);
+//     }
+//     return 0;
+// }
